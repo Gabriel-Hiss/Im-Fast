f566d7c0251648673fd66aa804c6001209bc3a56
Gabriel Monteiro <bielpaes912@gmail.com>
2025-07-04 10:44:47 -0300

    Port Requested by issue #14

---
+++ src/main/java/com/biel/mod/imfast.java
@@ -0,0 +1,9 @@
+package com.biel.mod;
+
+import net.minecraftforge.fml.common.Mod;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(imfast.MODID)
+public class imfast {
+    public static final String MODID = "imfast";
+}
