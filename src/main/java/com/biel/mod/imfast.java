033200b3cf616c2f390847eafc322a2feda08bce
Gabriel-Hiss <bielpaes912@gmail.com>
2025-10-28 10:03:27 -0300

    chore: port request from issue #17

---
+++ src/main/java/com/biel/mod/imfast.java
@@ -0,0 +1,9 @@
+package com.biel.mod;
+
+import net.neoforged.fml.common.Mod;
+
+// The value here should match an entry in the META-INF/neoforge.mods.toml file
+@Mod(imfast.MODID)
+public class imfast {
+    public static final String MODID = "imfast";
+}
