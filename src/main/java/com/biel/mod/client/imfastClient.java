8eeac4b068dda14023e0fab2168cd7057eb8e622
Gabriel-Hiss <bielpaes912@gmail.com>
2025-12-20 23:02:19 -0300

    chore: port request from issue #18

---
+++ src/main/java/com/biel/mod/client/imfastClient.java
@@ -0,0 +1,10 @@
+package com.biel.mod.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class imfastClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
