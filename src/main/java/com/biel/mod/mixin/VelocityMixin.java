2c845b8f0d30c9346fb858a30946faa64a4e1565
Gabriel-Hiss <bielpaes912@gmail.com>
2026-08-27 09:46:39 -0300

    fix: fix issue #28
    Co-authored-by: iain.ross2027@gmail.com

--- src/main/java/com/biel/mod/mixin/VelocityMixin.java
+++ src/main/java/com/biel/mod/mixin/VelocityMixin.java
@@ -8,7 +8,7 @@ import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
 
 
-@Mixin(ServerGamePacketListenerImpl.class)
+@Mixin(value = ServerGamePacketListenerImpl.class, priority = 2000)
 public class VelocityMixin {
     // Moved Quickly
     @ModifyConstant(method = "handleMovePlayer", constant = @Constant(floatValue = 100.0F))
@@ -35,7 +35,7 @@ public class VelocityMixin {
     }
 
     // Moved Wrongly
-    @ModifyConstant(method = "handleMoveVehicle", constant = @Constant(doubleValue = 0.0625))
+    @ModifyConstant(method = "handleMoveVehicle", constant = @Constant(doubleValue = 0.0625, ordinal = 1))
     private double imfast_VehicleMovedWrong(double speed) {
         return Double.MAX_VALUE;
     }
